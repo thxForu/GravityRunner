420d0ed0340b765a5bfac530ac3dec57c5716fa0
thxForu <xavavka12345@gmail.com>
2020-08-20 00:28:55 +0300

    Add dodge comet quest

---
+++ Assets/Scripts/Triggers/DodgeComet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeComet : MonoBehaviour
+{
+    public static int cometDodge;
+    void Start()
+    {
+        GameEvents.current.OnMoneyChange += DodgeCometCounter;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+            GameEvents.current.DodgeComet();
+    }
+
+    void DodgeCometCounter()
+    {
+        cometDodge += 1;
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.current.OnMoneyChange -= DodgeCometCounter;
+    }
+}
