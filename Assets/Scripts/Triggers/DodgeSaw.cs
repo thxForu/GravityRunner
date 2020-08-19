eef1d261117d0fe7fbbf2fe219d3ac937bfc1347
thxForu <xavavka12345@gmail.com>
2020-08-20 01:11:32 +0300

    add dodge quest

---
+++ Assets/Scripts/Triggers/DodgeSaw.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeSaw : MonoBehaviour
+{
+    public static int sawDodge;
+    void Start()
+    {
+        GameEvents.current.OnMoneyChange += DodgeSawCounter;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+            GameEvents.current.DodgeSaw();
+    }
+
+    void DodgeSawCounter()
+    {
+        sawDodge += 1;
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.current.OnMoneyChange -= DodgeSawCounter;
+    }
+}
